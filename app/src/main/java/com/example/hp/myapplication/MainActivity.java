f6f0c42cfd89530830ca6732de92ebd730edddd6
bin <1095274971@qq.com>
2016-02-06 14:37:45 +0800

    添加Log.d

--- app/src/main/java/com/example/hp/myapplication/MainActivity.java
+++ app/src/main/java/com/example/hp/myapplication/MainActivity.java
@@ -13,6 +13,7 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Log.d("this", "debug");
+        Log.d("Commit", "pull" );
     }
 
 }
