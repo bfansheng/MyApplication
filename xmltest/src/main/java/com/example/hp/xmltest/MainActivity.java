3e458826f226814f49e13fbcc90099fa30d7b949
bin <1095274971@qq.com>
2016-02-08 19:41:50 +0800

    XMLTest

--- xmltest/src/main/java/com/example/hp/xmltest/MainActivity.java
+++ xmltest/src/main/java/com/example/hp/xmltest/MainActivity.java
@@ -1,15 +1,43 @@
 package com.example.hp.xmltest;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int SHOW_RESPONSE = 0;
+    private Button sendRequest;
+    private TextView responseText;
+    //Handler消息处理
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_RESPONSE:
+                    String response = (String) msg.obj;
+                    responseText.setText(response);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +54,89 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        //获取按钮
+        responseText = (TextView) findViewById(R.id.response_text);
+        sendRequest = (Button) findViewById(R.id.send_request);
+        sendRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendRequestWithHttpURLConnection();
+            }
+        });
+    }
+
+    //响应网络请求
+    private void sendRequestWithHttpURLConnection() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                try {
+                    URL url = new URL("http://flash.weather.com.cn/wmaps/xml/fuzhou.xml");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    String result = response.toString();
+                    Log.i("MainActivity", result);
+                    //解析response;
+                    parseXMLWithPull(result);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            while (eventType != xmlPullParser.END_DOCUMENT) {
+                switch (eventType) {
+                    //开始标签
+                    case XmlPullParser.START_TAG:
+                        //获取标签名
+                        String tagName = xmlPullParser.getName();
+                        //Log.i("------", tagName);
+                        //获取索引为0的属性值
+                        String cityX = xmlPullParser.getAttributeValue(0);
+                        //Log.i("------", cityX);
+                        if (cityX.equals("400")) {
+                            String cityName = xmlPullParser.getAttributeValue(2);
+                            String stateDetailed = xmlPullParser.getAttributeValue(8);
+                            String temp1 = xmlPullParser.getAttributeValue(9);
+                            String temp2 = xmlPullParser.getAttributeValue(10);
+                            String response = cityName + "\n" + "天气：" + stateDetailed + "\n" + "温度：" + temp1 + "~" + temp2 + "℃";
+                            //Log.i("-------", response);
+                            Message msg = new Message();
+                            msg.what = SHOW_RESPONSE;
+                            msg.obj = response.toString();
+                            handler.sendMessage(msg);
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
