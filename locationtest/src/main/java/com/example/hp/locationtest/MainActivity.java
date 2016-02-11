926b0c796049a4e638cfe0936c7913eabafa3895
bin <1095274971@qq.com>
2016-02-11 15:27:03 +0800

    位置解析

--- locationtest/src/main/java/com/example/hp/locationtest/MainActivity.java
+++ locationtest/src/main/java/com/example/hp/locationtest/MainActivity.java
@@ -1,15 +1,38 @@
 package com.example.hp.locationtest;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int SHOW_LOCATION = 0;
+    private TextView positionTextView;
+    private LocationManager locationManager;
+    private String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +49,117 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        positionTextView = (TextView) findViewById(R.id.position_text_view);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        //获取所有可用的位置提供器
+        List<String> providerList = locationManager.getProviders(true);
+        if (providerList.contains(LocationManager.GPS_PROVIDER)) {
+            provider = LocationManager.GPS_PROVIDER;
+        } else if (providerList.contains(LocationManager.NETWORK_PROVIDER)) {
+            provider = LocationManager.NETWORK_PROVIDER;
+        } else {
+            Toast.makeText(this, "No location provider to use", Toast.LENGTH_LONG).show();
+        }
+        Location location = locationManager.getLastKnownLocation(provider);
+        if (location != null) {
+            //显示当前设备的位置信息
+            showLocation(location);
+        }
+        locationManager.requestLocationUpdates(provider, 5000, 1, locationListener);
     }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        if (locationManager != null) {
+            //关闭程序时将监听器移除
+            locationManager.removeUpdates(locationListener);
+        }
+    }
+
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            //更新当前设备的位置信息
+            showLocation(location);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+
+        }
+    };
+
+    private void showLocation(final Location location) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection httpURLConnection = null;
+                try {
+                    //组装法相地理编码的接口地址
+                    StringBuilder url = new StringBuilder();
+                    url.append("http://maps.googleapis.com/maps/api/geocode/json?latlng=");
+                    url.append(location.getLatitude()).append(",");
+                    url.append(location.getLongitude());
+                    url.append("&sensor=false");
+                    URL url1 = new URL(url.toString());
+                    //Log.i("------", url.toString());
+                    //需要由URL对象开启连接
+                    httpURLConnection = (HttpURLConnection) url1.openConnection();
+                    httpURLConnection.setRequestMethod("GET");
+                    httpURLConnection.setConnectTimeout(8000);
+                    httpURLConnection.setReadTimeout(8000);
+                    //设置语言，保证服务器返回中文数据
+                    httpURLConnection.setRequestProperty("Accept-Language", "zh-CN");
+                    InputStream inputStream = httpURLConnection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    //Log.i("------", response.toString());
+                    JSONObject jsonObject = new JSONObject(response.toString());
+                    JSONArray resultArray = jsonObject.getJSONArray("results");
+                    if (resultArray.length()>0) {
+                        JSONObject subObject = resultArray.getJSONObject(0);
+                        String adress = subObject.getString("formatted_address");
+                        Message message = new Message();
+                        message.what = SHOW_LOCATION;
+                        message.obj = adress;
+                        handler.sendMessage(message);
+                    }
+                }catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        String currentPosition = "latitude is" + location.getLatitude() + "\n" + "longitude is" + location.getLongitude();
+        positionTextView.setText(currentPosition);
+    }
+
+    private Handler handler = new Handler() {
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case SHOW_LOCATION:
+                    String currentPosition = (String) msg.obj;
+                    positionTextView.setText(currentPosition);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
