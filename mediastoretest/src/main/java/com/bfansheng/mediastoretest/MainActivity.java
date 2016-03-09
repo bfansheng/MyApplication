ff505709edd5bb6ab2d880e96b6035f4090afadf
bin <1095274971@qq.com>
2016-03-09 19:21:12 +0800

    mediatest

---
+++ mediastoretest/src/main/java/com/bfansheng/mediastoretest/MainActivity.java
@@ -0,0 +1,70 @@
+package com.bfansheng.mediastoretest;
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        Cursor cursor = getContentResolver()
+                .query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                        new String[]{MediaStore.Audio.Media.TITLE,
+                                MediaStore.Audio.Media.DURATION,
+                                MediaStore.Audio.Media.ALBUM,
+                                MediaStore.Audio.Media.ARTIST,
+                                MediaStore.Audio.Media._ID,
+                                MediaStore.Audio.Media.DATA,
+                                MediaStore.Audio.Media.DISPLAY_NAME}, null,
+                        null, null);
+        if (cursor != null && cursor.moveToFirst()) {
+            String mArtist = cursor.getString(3);
+            Log.i("musicName", mArtist);
+        }
+        cursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
